7916a406db4a01ba7e37ffca51175fdef79127e1
DESKTOP-FEK629B\高震育 <ae6013>
2017-11-30 15:41:59 +0800

    ~~

--- lineChat/index.js
+++ lineChat/index.js
@@ -66,7 +66,7 @@ bot.on('message', function(event) {
 			if(response.result.metadata.intentName=='find_singer - custom'){
 			console.log('find_singer - custom');
 			var check = checkexist(singer,param['song.original']);
-			console.log(checkexist(singer,param['song.original']));
+			console.log('123'+checkexist(singer,param['song.original']));
 			console.log('check~~true');
 			}
 			}).catch(function(error) {
